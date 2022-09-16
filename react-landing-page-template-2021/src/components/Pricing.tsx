33b329ee3596f6503eddf36d7d730bf0fc3b1f35
amy <afelix51228@gmail.com>
2022-09-15 22:51:17 -0600

    COLOR AND PICTURE

--- react-landing-page-template-2021/src/components/Pricing.tsx
+++ react-landing-page-template-2021/src/components/Pricing.tsx
@@ -2,54 +2,125 @@ import React from 'react';
 
 import config from '../config/index.json';
 
-const Team = () => {
-  const { features } = config;
-  const { title, subtitle, description, items: featuresList } = features;
-  return (
-      <div className={`py-12 bg-background`} id="features">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="lg:text-center">
-            <h2
-                className={`text-base text-primary font-semibold tracking-wide uppercase`}
-            >
-              {title}
-            </h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              {subtitle}
-            </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              {description}
-            </p>
-          </div>
+const Pricing = () => {
+  const { pricing } = config;
+  const { items, title } = pricing;
+  const [firstPlan, secondPlan, thirdPlan] = items;
 
-          <div className="mt-10">
-            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-              {featuresList.map((feature) => (
-                  <div key={feature.name} className="relative">
-                    <dt>
-                      <div
-                          className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-primary border-4`}
+  return (
+      <section className={`bg-background py-8`} id="pricing">
+        <div className={`container mx-auto px-2 pt-4 pb-12 text-primary`}>
+          <h1
+              className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
+          >
+            {title}
+          </h1>
+          <div className={`w-full mb-4`}>
+            <div
+                className={`h-1 mx-auto bg-primary w-64 opacity-25 my-0 py-0 rounded-t`}
+            ></div>
+          </div>
+          <div
+              className={`flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4`}
+          >
+            <div
+                className={`flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-background mt-4`}
+            >
+              <div
+                  className={`flex-1 bg-background text-gray-600 rounded-t rounded-b-none overflow-hidden shadow`}
+              >
+                <div className={`p-8 text-3xl font-bold text-center border-b-4`}>
+                  {firstPlan?.name}
+                </div>
+                <ul className={`w-full text-center text-sm`}>
+                  {firstPlan?.features.map((feature) => (
+                      <li
+                          className={`border-b py-4`}
+                          key={`${firstPlan.name}-${feature}`}
                       >
-                        <img
-                            className={`inline-block h-6 w-6 rounded-full`}
-                            src={feature.icon}
-                            alt={feature.name}
-                        />
-                      </div>
-                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                        {feature.name}
-                      </p>
-                    </dt>
-                    <dd className="mt-2 ml-16 text-base text-gray-500">
-                      {feature.description}
-                    </dd>
-                  </div>
-              ))}
-            </dl>
+                        {feature}
+                      </li>
+                  ))}
+                </ul>
+              </div>
+              <div
+                  className={`flex-none mt-auto bg-background rounded-b rounded-t-none overflow-hidden shadow p-6`}
+              >
+                <div
+                    className={`w-full pt-6 text-3xl text-gray-600 font-bold text-center`}
+                >
+                  {firstPlan?.price}
+                  <span className={`text-base`}> {firstPlan?.priceDetails}</span>
+                </div>
+              </div>
+            </div>
+            <div
+                className={`flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-background mt-4 sm:-mt-6 shadow-lg z-10`}
+            >
+              <div
+                  className={`flex-1 bg-background rounded-t rounded-b-none overflow-hidden shadow`}
+              >
+                <div className={`w-full p-8 text-3xl font-bold text-center`}>
+                  {secondPlan?.name}
+                </div>
+                <div
+                    className={`h-1 w-full bg-primary my-0 py-0 rounded-t`}
+                ></div>
+                <ul className={`w-full text-center text-base font-bold`}>
+                  {secondPlan?.features.map((feature) => (
+                      <li
+                          className={`border-b py-4`}
+                          key={`${secondPlan?.name}-${feature}`}
+                      >
+                        {feature}
+                      </li>
+                  ))}
+                </ul>
+              </div>
+              <div
+                  className={`flex-none mt-auto bg-background rounded-b rounded-t-none overflow-hidden shadow p-6`}
+              >
+                <div className={`w-full pt-6 text-4xl font-bold text-center`}>
+                  {secondPlan?.price}
+                  <span className={`text-base`}> {secondPlan?.priceDetails}</span>
+                </div>
+              </div>
+            </div>
+            <div
+                className={`flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-primary mt-4`}
+            >
+              <div
+                  className={`flex-1 bg-background text-gray-600 rounded-t rounded-b-none overflow-hidden shadow`}
+              >
+                <div className={`p-8 text-3xl font-bold text-center border-b-4`}>
+                  {thirdPlan?.name}
+                </div>
+                <ul className={`w-full text-center text-sm`}>
+                  {thirdPlan?.features.map((feature) => (
+                      <li
+                          className={`border-b py-4`}
+                          key={`${thirdPlan?.name}-${feature}`}
+                      >
+                        {feature}
+                      </li>
+                  ))}
+                </ul>
+              </div>
+              <div
+                  className={`flex-none mt-auto bg-background rounded-b rounded-t-none overflow-hidden shadow p-6`}
+              >
+                <div
+                    className={`w-full pt-6 text-3xl text-gray-600 font-bold text-center`}
+                >
+                  {thirdPlan?.price}
+                  <span className={`text-base`}> {thirdPlan?.priceDetails}</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
   );
 };
 
-export default Team;
+export default Pricing;
--- react-landing-page-template-2021/tailwind.config.js
+++ react-landing-page-template-2021/tailwind.config.js
@@ -17,8 +17,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#ec4755',
-        secondary: '#a12c34',
+        primary: '#d347ec',
+        secondary: '#9d24b4',
         tertiary: '#99a0a3',
         border: '#1a2e35',
         background: '#ffffff',
