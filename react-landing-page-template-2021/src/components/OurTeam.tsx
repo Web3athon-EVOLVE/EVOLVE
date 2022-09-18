1887f9e7feb5b1a993a412b3639c288b7d7d6ecc
Amy <afelix51228@gmail.com>
2022-09-17 22:43:33 -0600

    Saturday's progress

--- react-landing-page-template-2021/src/components/OurTeam.tsx
+++ react-landing-page-template-2021/src/components/OurTeam.tsx
@@ -14,11 +14,59 @@ const OurTeam = () => {
                 </h1>
             </div>
             <h2 className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}>
-                <div>
-                    <img src={"/assets/images/angele.png"}/>
-                    hello
-                    <img src={"/assets/images/angele.png"}/>
+                <div className={`row`}>
+                    <div className={`column`}>
+                    <img src={`/assets/images/olivia.png`} />
+                        <div className={`font`}>Angele</div>
+                        <div className={`font-2`}>Lead & Project Manager:</div>
+                        <div className={`font-2`}>Creator | Investor I Entrepreneur </div>
+                    </div>
+                    <div className={`column`}>
+                        <img src={`/assets/images/zawadi.png`} />
+                        <div className={`font`}>Zawadi</div>
+                        <div className={`font-2`}>Product Management</div>
+                        <div className={`font-2`}>Storyteller </div>
+                    </div>
+                    <div className={`column`}>
+                        <img src={`/assets/images/angele.png`} />
+                        <div className={`font`}>Angelé</div>
+                        <div className={`font-2`}>Strategist</div>
+                        <div className={`font-2`}>Innovator | Cultivator </div>
+                    </div>
+                    <div className={`column`}>
+                        <img src={`/assets/images/evelyn.png`} />
+                        <div className={`font`}>Evelyn</div>
+                        <div className={`font-2`}>Creator</div>
+                        <div className={`font-2`}>Investor | Consultant </div>
+                    </div>
+
                 </div>
+                <h2>
+                <div className={`row`}>
+                    <div className={`column`}>
+                        <div className={`column`}>
+                            <img src={`/assets/images/shelia.png`} />
+                            <div className={`font`}>Evelyn</div>
+                            <div className={`font-2`}>Creator</div>
+                            <div className={`font-2`}>Dao Founder | Ideation </div>
+
+                        </div>
+                        <div className={`column`}>
+                            <img src={`/assets/images/diania.jpeg`} />
+                            <div className={`font`}>Diana</div>
+                            <div className={`font-2`}>Fullstack Web Developer</div>
+                            <div className={`font-2`}>Blockchain Engineer </div>
+                            <div className={`font-2`}>H.E.R. DAO Contributor</div>
+                        </div>
+                        <div className={`column`}>
+                            <img src={`/assets/images/amy.jpeg`} />
+                            <div className={`font`}>Amy</div>
+                            <div className={`font-2`}>Fullstack Engineer</div>
+                            <div className={`font-2`}>Associate Technical Consultant </div>
+                        </div>
+                    </div>
+                </div>
+                </h2>
             </h2>
         </section>
     );
